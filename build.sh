15d39e6cd8fde585f31716cd75612feaa4752f04
cm402 <37273900+cm402@users.noreply.github.com>
2021-04-09 22:00:11 +0100

    Updating shell scripts

--- build.sh
+++ build.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
 THE_CLASSPATH=
-PROGRAM_NAME=GameEngine
 cd src
 for i in `ls ../lib/*.jar`
     do
     THE_CLASSPATH=${THE_CLASSPATH}:${i}
 done
 
-java -cp ".:${THE_CLASSPATH}" $PROGRAM_NAME $1
+javac -classpath ".:${THE_CLASSPATH}" *.java
 
 if [ $? -eq 0 ]
 then
